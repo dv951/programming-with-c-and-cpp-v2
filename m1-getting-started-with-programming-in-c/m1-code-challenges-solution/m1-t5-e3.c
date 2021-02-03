b6e9209b1f7e78ea30fa8938a5c294596951b4b5
dv951 <78480805+dv951@users.noreply.github.com>
2021-02-03 17:13:17 +0530

    Update m1-t5-e3.c

--- m1-getting-started-with-programming-in-c/m1-code-challenges-solution/m1-t5-e3.c
+++ m1-getting-started-with-programming-in-c/m1-code-challenges-solution/m1-t5-e3.c
@@ -1,10 +1,10 @@
     #include <stdio.h>
 
     int main() {
-
-        puts("I am Rahul.");
-        puts("I am 20 years old.");
-
-        return 0;
-    }
+        
+        puts(" my name is vivek \n");
+        puts(" and i am 24 year old.");
+        
+        return 0
+    }            
 
